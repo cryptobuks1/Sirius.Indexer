97720ddb48a437873da4acdde93536e33ef5cf34
KonstantinRyazantsev <kj.ryazancev@gmail.com>
2020-03-27 19:07:42 +0300

    Template updated

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Sirius generic blockchain indexer
 
-![Continous integration build and publish](https://github.com/SC-Poc/Sirius.Indexer/workflows/Continous%20integration%20build%20and%20publish/badge.svg)
+![Continous integration build and publish](https://github.com/swisschain/Sirius.Indexer/workflows/Continous%20integration%20build%20and%20publish/badge.svg)
 
 ![API docker image](https://img.shields.io/docker/v/swisschains/sirius-indexer?sort=semver)
 ![Worker docker image](https://img.shields.io/docker/v/swisschains/sirius-indexer-worker?sort=semver)
--- src/Indexer/Program.cs
+++ src/Indexer/Program.cs
@@ -1,42 +1,53 @@
 ﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Swisschain.Sdk.Server.Common;
-using Swisschain.Sdk.Server.Loggin;
+using Swisschain.Sdk.Server.Logging;
 
 namespace Indexer
 {
     public class Program
     {
+        private sealed class RemoteSettingsConfig
+        {
+            public IReadOnlyCollection<string> RemoteSettingsUrls { get; set; }
+        }
+
         public static void Main(string[] args)
         {
             Console.Title = "Sirius Indexer";
 
-            using (var loggerFactory = LogConfigurator.Configure("Sirius", ApplicationEnvironment.Config["SeqUrl"]))
+            var remoteSettingsConfig = ApplicationEnvironment.Config.Get<RemoteSettingsConfig>();
+
+            using var loggerFactory = LogConfigurator.Configure(
+                "Sirius", 
+                ApplicationEnvironment.Config["SeqUrl"],
+                remoteSettingsConfig.RemoteSettingsUrls ?? Array.Empty<string>());
+            
+            var logger = loggerFactory.CreateLogger<Program>();
+
+            try
             {
-                var logger = loggerFactory.CreateLogger<Program>();
+                logger.LogInformation("Application is being started");
 
-                try
-                {
-                    logger.LogInformation("Application is being started");
+                CreateHostBuilder(loggerFactory, remoteSettingsConfig).Build().Run();
 
-                    CreateHostBuilder(loggerFactory).Build().Run();
-
-                    logger.LogInformation("Application has been stopped");
-                }
-                catch (Exception ex)
-                {
-                    logger.LogCritical(ex, "Application has been terminated unexpectedly");
-                }
+                logger.LogInformation("Application has been stopped");
+            }
+            catch (Exception ex)
+            {
+                logger.LogCritical(ex, "Application has been terminated unexpectedly");
             }
         }
 
-        private static IHostBuilder CreateHostBuilder(ILoggerFactory loggerFactory) =>
+        private static IHostBuilder CreateHostBuilder(ILoggerFactory loggerFactory, RemoteSettingsConfig remoteSettingsConfig) =>
             new HostBuilder()
                 .SwisschainService<Startup>(options =>
                 {
                     options.UseLoggerFactory(loggerFactory);
-                    options.WithWebJsonConfigurationSource(ApplicationEnvironment.Config["RemoteSettingsUrl"]);
+                    options.AddWebJsonConfigurationSources(remoteSettingsConfig.RemoteSettingsUrls ?? Array.Empty<string>());
                 });
     }
 }
