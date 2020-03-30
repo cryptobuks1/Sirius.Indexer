931cf2cb3a4e582e496fcbef5b6b480d5b967761
Aleksandr Belousov <alexandr.belousov@lykke.com>
2020-03-30 20:20:53 +0400

    Add TransactionDetected event

---
+++ src/Indexer.MessagingContract/Transfer.cs
@@ -0,0 +1,21 @@
+﻿using Swisschain.Sirius.Sdk.Primitives;
+
+namespace Swisschain.Sirius.Indexer.MessagingContract
+{
+    public class Transfer
+    {
+        public int TransferId { get; set; }
+        public decimal Amount { get; set; }
+
+        /// <summary>
+        /// Optional
+        /// </summary>
+        public DestinationTag Tag { get; set; }
+
+        /// <summary>
+        /// Optional
+        /// </summary>
+        public DestinationTagType? TagType { get; set; }
+        public long Nonce { get; set; }
+    }
+}
